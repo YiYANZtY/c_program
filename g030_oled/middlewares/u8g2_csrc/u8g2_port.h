ea3acf4336f969895c087fedb3235f966baadfa6
YiYANZtY <45869756+YiYANZtY@users.noreply.github.com>
2023-02-01 17:18:25 +0800

    修复u8g2显示相差2个像素的问题 移植mono ui 暂时没有完成key和adc部分

--- g030_oled/middlewares/u8g2_csrc/u8g2_port.h
+++ g030_oled/middlewares/u8g2_csrc/u8g2_port.h
@@ -4,6 +4,9 @@
 #include "main.h"
 #include "u8g2.h"
 
+#define CONFIG_SCREEN_HEIGHT 64   // height of screen
+#define CONFIG_SCREEN_WIDTH  128  // width of screen
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,6 +16,7 @@ extern I2C_HandleTypeDef hi2c1;
 uint8_t u8x8_byte_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 uint8_t u8x8_gpio_and_delay_stm32(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int,
     void *arg_ptr);
+void u8g2Init(u8g2_t* u8g2);
 
 #ifdef __cplusplus
 }
